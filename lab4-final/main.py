c4c19607d07d076af7c6d99615aab04c79953c4d
Daniel Znaceni <44608412+daniel357@users.noreply.github.com>
2023-11-24 12:40:01 +0200

    Add files via upload

---
+++ lab4-final/main.py
@@ -0,0 +1,34 @@
+# This is a sample Python script.
+from Scanner import Scanner
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def test_p1():
+    file = open('p1.txt', 'r')
+    scanner = Scanner(file.read())
+
+
+def test_p1error():
+    file = open('p1error.txt', 'r')
+    scanner = Scanner(file.read())
+
+
+def test_p2():
+    file = open('p2.txt', 'r')
+    scanner = Scanner(file.read())
+
+
+def test_p3():
+    file = open('p3.txt', 'r')
+    scanner = Scanner(file.read())
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    test_p1()
+    # test_p2()
+    # test_p3()
+    # test_p1error()
